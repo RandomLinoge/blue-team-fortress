ee5e34b4b691f92a9427a7e9977058b5a0a9691d
Bakeneko <68653785+RandomLinoge@users.noreply.github.com>
2025-07-15 00:58:43 +0300

    Update artifact_query.sql

--- velociraptor/artifact_query.sql
+++ velociraptor/artifact_query.sql
@@ -1,1 +1,5 @@
-# Sample content for artifact_query.sql
+-- Sample Velociraptor artifact query
+SELECT EventTime, CommandLine, ParentProcess
+FROM source()
+WHERE CommandLine CONTAINS 'Invoke-Mimikatz'
+AND EventID = 1
